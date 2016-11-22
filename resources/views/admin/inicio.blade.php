f77403a643d0d6a571b35e487c210561dd8b6195
mark <marks88martinez@gmail.com>
2016-11-22 15:34:26 -0300

    modificacion

--- resources/views/admin/inicio.blade.php
+++ resources/views/admin/inicio.blade.php
@@ -9,7 +9,7 @@
     {{--<link rel="icon" type="image/png" href="assets/img/favicon-16x16.png" sizes="16x16">--}}
     {{--<link rel="icon" type="image/png" href="assets/img/favicon-32x32.png" sizes="32x32">--}}
 
-    <title>Altair Admin v1.0.0</title>
+    <title>Programa Abrazo</title>
 
     <!-- additional styles for plugins -->
     <!-- weather icons -->
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <link rel="icon" type="image/png" href="assets/img/favicon-16x16.png" sizes="16x16">
-    <link rel="icon" type="image/png" href="assets/img/favicon-32x32.png" sizes="32x32">
+    {{--<link rel="icon" type="image/png" href="assets/img/favicon-16x16.png" sizes="16x16">--}}
+    {{--<link rel="icon" type="image/png" href="assets/img/favicon-32x32.png" sizes="32x32">--}}
 
-    <title>Altair Admin v1.0.0 - Login Page</title>
+    <title>Programa Abrazo</title>
 
     <link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500' rel='stylesheet' type='text/css'>
 
@@ -56,29 +56,29 @@
                 {{--</div>--}}
             {!! Form::close() !!}
         </div>
-        <div class="md-card-content large-padding uk-position-relative" id="login_help" style="display: none">
-            <button type="button" class="uk-position-top-right uk-close uk-margin-right uk-margin-top" id="login_help_close"></button>
-            <h2 class="heading_b uk-text-success">Can't log in?</h2>
-            <p>Here’s the info to get you back in to your account as quickly as possible.</p>
-            <p>First, try the easiest thing: if you remember your password but it isn’t working, make sure that Caps Lock is turned off, and that your username is spelled correctly, and then try again.</p>
-            <p>If your password still isn’t working, it’s time to <a href="#" id="login_password_reset_show">reset your password</a>.</p>
-        </div>
-        <div class="md-card-content large-padding" id="login_password_reset" style="display: none">
-            <h2 class="heading_a uk-margin-large-bottom">Reset password</h2>
-            <form action="">
-                <div class="uk-form-row">
-                    <label for="login_email_reset">Your email address</label>
-                    <input class="md-input" type="text" id="login_email_reset" name="login_email_reset" />
-                </div>
-                <div class="uk-margin-medium-top">
-                    <button class="md-btn md-btn-primary md-btn-block">Reset password</button>
-                </div>
-            </form>
-        </div>
-    </div>
-    <div class="uk-margin-top">
-        <a href="#" id="login_help_show">Need help?</a>
-    </div>
+        {{--<div class="md-card-content large-padding uk-position-relative" id="login_help" style="display: none">--}}
+            {{--<button type="button" class="uk-position-top-right uk-close uk-margin-right uk-margin-top" id="login_help_close"></button>--}}
+            {{--<h2 class="heading_b uk-text-success">Can't log in?</h2>--}}
+            {{--<p>Here’s the info to get you back in to your account as quickly as possible.</p>--}}
+            {{--<p>First, try the easiest thing: if you remember your password but it isn’t working, make sure that Caps Lock is turned off, and that your username is spelled correctly, and then try again.</p>--}}
+            {{--<p>If your password still isn’t working, it’s time to <a href="#" id="login_password_reset_show">reset your password</a>.</p>--}}
+        {{--</div>--}}
+        {{--<div class="md-card-content large-padding" id="login_password_reset" style="display: none">--}}
+            {{--<h2 class="heading_a uk-margin-large-bottom">Reset password</h2>--}}
+            {{--<form action="">--}}
+                {{--<div class="uk-form-row">--}}
+                    {{--<label for="login_email_reset">Your email address</label>--}}
+                    {{--<input class="md-input" type="text" id="login_email_reset" name="login_email_reset" />--}}
+                {{--</div>--}}
+                {{--<div class="uk-margin-medium-top">--}}
+                    {{--<button class="md-btn md-btn-primary md-btn-block">Reset password</button>--}}
+                {{--</div>--}}
+            {{--</form>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="uk-margin-top">--}}
+        {{--<a href="#" id="login_help_show">Need help?</a>--}}
+    {{--</div>--}}
 </div>
 
 <!-- common functions -->
