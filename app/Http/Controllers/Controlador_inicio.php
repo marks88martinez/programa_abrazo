2950fad1c23cadfd2ffbc0bbd09a9970b4d9fc17
mark <marks88martinez@gmail.com>
2016-12-09 12:03:36 -0300

    Agregado dd de prueba en graficos

--- app/Http/Controllers/Controlador_inicio.php
+++ app/Http/Controllers/Controlador_inicio.php
@@ -49,7 +49,7 @@ class Controlador_inicio extends Controller
 //        dd($dimensionesPorDia["2016-11-09"]->where('dimensiones_t', 2)[0]->cantidad);
         $fechas = $dimensionesPorDia->keys();
         $todColl = $dimensionesPorDia["2016-12-09"];
-        dd($todColl);
+//        dd($todColl);
         dd($todColl->where('fecha', "2016-12-09"));
         return view('admin.graficos', compact('horasPorDia', 'dimensiones', 'dimensionesPorDia', 'fechas'));
     }
